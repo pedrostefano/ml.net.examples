cebc65a83ea121de514ccea38d4c4e9c546be25d
Pedro Stefano Toffalini Fernandes <pedrostefano03@gmail.com>
2019-12-22 22:35:20 -0300

    IrisFlowerClustering

---
+++ IrisFlowerClustering/TestIrisData.cs
@@ -0,0 +1,10 @@
+static class TestIrisData
+{
+  internal static readonly IrisData Setosa = new IrisData
+  {
+    SepalLength = 5.1f,
+    SepalWidth = 3.5f,
+    PetalLength = 1.4f,
+    PetalWidth = 0.2f
+  };
+}
