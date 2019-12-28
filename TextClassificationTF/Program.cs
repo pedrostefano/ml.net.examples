ab7f49c4fd7a5395aba5cadac16f27db10700a6e
Pedro Stefano Toffalini Fernandes <pedrostefano03@gmail.com>
2019-12-28 17:26:50 -0300

    TextClassificationTF

---
+++ TextClassificationTF/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms;
+
+namespace TextClassificationTF
+{
+  class Program
+  {
+    static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "sentiment_model");
+    public const int FeatureLength = 600;
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      MLContext mlContext = new MLContext();
+
+      var lookupMap = mlContext.Data.LoadFromTextFile(Path.Combine(_modelPath, "imdb_word_index.csv"),
+        columns: new[]
+          {
+                new TextLoader.Column("Words", DataKind.String, 0),
+                new TextLoader.Column("Ids", DataKind.Int32, 1),
+          },
+        separatorChar: ','
+      );
+      Action<VariableLength, FixedLength> ResizeFeaturesAction = (s, f) =>
+      {
+        var features = s.VariableLengthFeatures;
+        Array.Resize(ref features, FeatureLength);
+        f.Features = features;
+      };
+
+      TensorFlowModel tensorFlowModel = mlContext.Model.LoadTensorFlowModel(_modelPath);
+      DataViewSchema schema = tensorFlowModel.GetModelSchema();
+      Console.WriteLine(" =============== TensorFlow Model Schema =============== ");
+      var featuresType = (VectorDataViewType)schema["Features"].Type;
+      Console.WriteLine($"Name: Features, Type: {featuresType.ItemType.RawType}, Size: ({featuresType.Dimensions[0]})");
+      var predictionType = (VectorDataViewType)schema["Prediction/Softmax"].Type;
+      Console.WriteLine($"Name: Prediction/Softmax, Type: {predictionType.ItemType.RawType}, Size: ({predictionType.Dimensions[0]})");
+
+      IEstimator<ITransformer> pipeline = mlContext.Transforms.Text.TokenizeIntoWords("TokenizedWords", "ReviewText")
+        // Map each word to an integer value. The array of integer makes up the input features.
+        .Append(mlContext.Transforms.Conversion.MapValue("VariableLengthFeatures", lookupMap,
+            lookupMap.Schema["Words"], lookupMap.Schema["Ids"], "TokenizedWords"))
+        // Resize variable length vector to fixed length vector.
+        .Append(mlContext.Transforms.CustomMapping(ResizeFeaturesAction, "Resize"))
+        // Passes the data to TensorFlow for scoring
+        .Append(tensorFlowModel.ScoreTensorFlowModel("Prediction/Softmax", "Features"))
+        // Retrieves the 'Prediction' from TensorFlow and and copies to a column
+        .Append(mlContext.Transforms.CopyColumns("Prediction", "Prediction/Softmax"));
+
+      // Create an executable model from the estimator pipeline
+      IDataView dataView = mlContext.Data.LoadFromEnumerable(new List<MovieReview>());
+      ITransformer model = pipeline.Fit(dataView);
+
+      PredictSentiment(mlContext, model);
+    }
+    public static void PredictSentiment(MLContext mlContext, ITransformer model)
+    {
+      var engine = mlContext.Model.CreatePredictionEngine<MovieReview, MovieReviewSentimentPrediction>(model);
+      var review = new MovieReview()
+      {
+        ReviewText = "really beautiful movie"
+      };
+      var sentimentPrediction = engine.Predict(review);
+      Console.WriteLine("Number of classes: {0}", sentimentPrediction.Prediction.Length);
+      Console.WriteLine("Is sentiment/review positive? {0}", sentimentPrediction.Prediction[1] > 0.5 ? "Yes." : "No.");
+
+    }
+
+
+  }
+  public class MovieReview
+  {
+    public string ReviewText { get; set; }
+  }
+
+  public class VariableLength
+  {
+    [VectorType]
+    public int[] VariableLengthFeatures { get; set; }
+  }
+  public class FixedLength
+  {
+    [VectorType(600)]
+    public int[] Features { get; set; }
+  }
+  public class MovieReviewSentimentPrediction
+  {
+    [VectorType(2)]
+    public float[] Prediction { get; set; }
+  }
+}
